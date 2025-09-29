177c8f2b349140496893acd96b7627ed95395b42
shumilika <shumilika@gmail.com>
2025-09-29 15:11:59 +0200

    add rechart library

---
+++ src/components/admin/VisitorsStatistics.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VisitorsStatistics = () => {
+    return (
+        <div className='p-6 bg-white rounded-xl shadow-md xl:col-span-2 flex items-center w-full'>
+            <h1 className='text-xl font-semibold mb-4'>Visitor Statistics</h1>
+        </div>
+    );
+};
+
+export default VisitorsStatistics;
